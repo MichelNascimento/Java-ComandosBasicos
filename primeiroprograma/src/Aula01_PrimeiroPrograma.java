c28fede3d7feb665574f7ae568dab48cf18256e4
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2023-03-27 08:16:10 -0300

    Renomeando classes

---
+++ primeiroprograma/src/Aula01_PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+//package primeiroprograma
+
+//CAMEL CASE, a técnica de escrever palavras compostas no modo que cada palavra comece com a primeira letra MAIÚSCULA.
+
+/*Sempre que a primeiro letra estiver em maiuscula, ela indica uma CLASSE ou INTERFACE
+Ex. PrimeiroPrograma.*/
+
+/*Sempre qua a primeira letra for minúscula ele pode ser um ATRIBUTO, VARIÁVEL ou um MÉTODO.*/
+
+/*Palavras em MAIÚSCULA inidica um CONSTANATE.*/
+
+/*palavras em minúsula indica um pacote.*/
+
+
+public class Aula01_PrimeiroPrograma {  
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");        
+    }
+    
+}
